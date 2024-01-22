39a6e5250f31b897677739db456f8f6cad054e89
GeSamaras <georgiosamaras@hotmail.com>
2024-01-23 00:34:20 +0100

    users cool request

--- app.js
+++ app.js
@@ -13,17 +13,28 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+
+// this is the path where the templates will be stored
 app.set('views', path.join(__dirname, 'views'));
+// and this is for the template library pug
 app.set('view engine', 'pug');
 
+
+// these are the middleware functions that are used to process the requests
 app.use(logger('dev'));
+
+// this is the middleware function that is used to parse the request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// middleware for cookies and static files
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// this callback function is used to handle the HTTP request for the path /users
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  // could use the next function to pass the request to the next middleware function
 });
 
 module.exports = router;
